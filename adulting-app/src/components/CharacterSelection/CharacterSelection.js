485081c5bc279b5cd0db7e0b9fa9ac1a192e40b9
dychotomy <dychotomy2000@yahoo.com>
2020-08-18 09:49:11 -0400

    CSS finalized styling for character selection page

--- adulting-app/src/components/CharacterSelection/CharacterSelection.js
+++ adulting-app/src/components/CharacterSelection/CharacterSelection.js
@@ -1,22 +1,23 @@
 import React from 'react'
+import './character-selection.css'
 
 const CharacterSelection = (props) => {
 
     let display = props.characters.map((element, i) => {
         return (
-            <div key={element.id}>
-                <div>{element.name}</div>
-                <img src={element.artwork} alt={element.name} />
-                <div>{element.description}</div>
-                <button onClick={() => props.chooseCharacter(element.id)}>Pick me</button>
+            <div key={element.id} className='character'>
+                <div className='character-name'>{element.name}</div>
+                <img className='character-img' src={element.artwork} alt={element.name} />
+                <div className='character-desc'>{element.description}</div>
+                <button className='character-button' onClick={() => props.chooseCharacter(element.id)}>Pick me</button>
             </div>
         )
     })
     return (
-        <>
-            <h2>Choose your character</h2>
+        <div className='character-container'>
+            <h2 className='character-title'>Choose your character</h2>
             {display}
-        </>
+        </div>
     )
 }
 
