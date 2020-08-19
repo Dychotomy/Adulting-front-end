9ef06637f725c4089cc35eba13a6f1e63665a273
dychotomy <dychotomy2000@yahoo.com>
2020-08-18 20:46:55 -0400

    Added credits and end scene text

--- adulting-app/src/components/About/About.js
+++ adulting-app/src/components/About/About.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import './about.css'
@@ -9,10 +8,47 @@ const About = () => {
         <div className='about'>
             <Header />
             <div>
-                <h2>About the game concept</h2>
-                <p>
-                    Credits
+                <h2 className='about-title'>About</h2>
+                <p className='about-text'>
+                    Adulting is a game inspired by the likes of the Telltale game “Wolf Among Us” and the game “Yawgh”. Admittedly, a much, much, simpler game in scale and complexity. While many of the experiences Jack and Jill have to deal with are drawn from real life, please take everything in this game with a grain salt. Also cats hate water, please don’t abuse your cat.
                 </p>
+                <h3 className='about-credit'>Credits</h3>
+                <ul className='about-list'>
+                    <li>Vishnu Nair</li>
+                    <li>Milada Vigerova</li>
+                    <li>Ryan Chia</li>
+                    <li>Ian Stauffer</li>
+                    <li>Eiliv-Sonas Aceron</li>
+                    <li>Maria Lin Kim</li>
+                    <li>Enrico Mantegazz</li>
+                    <li>Paolo Nicolello</li>
+                    <li>Joshua Coleman</li>
+                    <li>Megan Markham</li>
+                    <li>Anastasiia Ostapovych</li>
+                    <li>Sharon McCutcheo</li>
+                    <li>Uliana Kopanytsia</li>
+                    <li>Bianca Jordan</li>
+                    <li>Shanna Camilleri</li>
+                    <li>Jason Leung</li>
+                    <li>Toine G</li>
+                    <li>Martha Dominquez</li>
+                    <li>Brittany Colette</li>
+                    <li>Bill Oxford</li>
+                    <li>Luis Villasmil</li>
+                    <li>Matt Popovich</li>
+                    <li>Scott Graham</li>
+                    <li>Christa Dodoo</li>
+                    <li>Wesley Tingey</li>
+                    <li>Nathan Dumlao</li>
+                    <li>Kazuky Akayashi</li>
+                    <li>Alex Chernenko</li>
+                    <li>Cassandra Hamer</li>
+                    <li>Daoud Abismail</li>
+                    <li>Denis Harschi</li>
+                    <li>Jasmin Chew</li>
+                    <li>Kenan Reed</li>
+
+                </ul>
             </div>
             <Footer />
         </div>
--- adulting-app/src/components/EndScene/EndScene.js
+++ adulting-app/src/components/EndScene/EndScene.js
@@ -11,28 +11,28 @@ const EndScene = (props) => {
         screen =
             <div>
                 <img className='end-img' src='https://res.cloudinary.com/dpn6ltns1/image/upload/v1597770425/Project%204/Events/endscene-win_fgybfm.jpg' alt='end credit' />
-                <p className='end-desc'>You made it!</p>
+                <p className='end-desc'>You have achieved your final form, an adult through and through. Go forth and celebrate with the smug satisfaction of knowing you made it through the week and ready for the next. </p>
 
             </div>
     } else if (player.resources > 30) {
         screen =
             <div>
                 <img className='end-img' src='https://res.cloudinary.com/dpn6ltns1/image/upload/v1597772381/Project%204/Events/endscene_concert_sanwbf.jpg' alt='end credit' />
-                <p className='end-desc'>You made it!</p>
+                <p className='end-desc'>Life is hard, but it’s not all that bad. Take the win and enjoy your weekend.</p>
 
             </div>
     } else if (player.resources > 20) {
         screen =
             <div>
                 <img className='end-img' src='https://res.cloudinary.com/dpn6ltns1/image/upload/v1597772377/Project%204/Events/endscene_fluffy_efxhtk.jpg' alt='end credit' />
-                <p className='end-desc'>You made it!</p>
+                <p className='end-desc'>At least Fluffy still loves you.</p>
 
             </div>
     } else {
         screen =
             <div>
                 <img className='end-img' src='https://res.cloudinary.com/dpn6ltns1/image/upload/v1597770422/Project%204/Events/endscene-loss_npf0ty.jpg' alt='end credit' />
-                <p className='end-desc'>You made it!</p>
+                <p className='end-desc'>It was a tough week, no one said being an adult would be easy. There is always next week to get better.</p>
 
             </div>
     }
